eff14ba1c88bab13bd37e0a5c4a1c657271581af
DESKTOP-6EIGSJM\Brian Zuber <zuber573@morrisville.edu>
2022-08-31 13:32:13 -0400

    C# Formatting & Input.GetAxis()

--- Mover.cs
+++ Mover.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class Mover : MonoBehaviour
 {
-    [SerializeField]float xValue = 0.01f;
-    [SerializeField]float yValue = 1;
-    [SerializeField]float zValue = 0;
+ 
+    
     
     // Start is called before the first frame update
+   
     void Start()
     {
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Translate(xValue,yValue,zValue);
+    {   
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue,0,zValue);
     }
 }
