d01cc51a67f60ff340c3cb750d0c03c72444e95f
DESKTOP-6EIGSJM\Brian Zuber <zuber573@morrisville.edu>
2022-08-31 13:47:56 -0400

    Time.deltaTime

--- Mover.cs
+++ Mover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
  
-    
+   float moveSpeed = 10f; 
     
     // Start is called before the first frame update
    
@@ -15,8 +15,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue,0,zValue);
     }
 }
