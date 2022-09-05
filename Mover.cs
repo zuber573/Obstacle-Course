feba55325f6a0203cfa12160afa6ac3ae984e498
DESKTOP-6EIGSJM\Brian Zuber <zuber573@morrisville.edu>
2022-09-05 13:37:29 -0400

    Practicing With Methods

--- Mover.cs
+++ Mover.cs
@@ -10,13 +10,27 @@ public class Mover : MonoBehaviour
    
     void Start()
     {
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+       Debug.Log("Welcome to the game");
+       Debug.Log("Move your plaer with WSAD");
+       Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
+
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue,0,zValue);
+        transform.Translate(xValue, 0, zValue);
     }
 }
